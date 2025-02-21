2e3bd117797ec197a5bae88629a7ec94011e548f
Francis2302 <francis.antony@promon.co.in>
2025-02-21 15:44:04 +0530

    models changes

--- Django_ORM/CRUD_APP/models.py
+++ Django_ORM/CRUD_APP/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+class ProductCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = "product_category"  # Custom table name
+
+    def __str__(self):
+        return f"Category {self.id}: {self.name}"
     
 
 
